b900f6e9f8028c6ce5508148d90f1bef8165fcba
vijay <vijay.singh@velocis.co.in>
2022-08-01 13:50:10 +0530

    Initial project setup

---
+++ SwiftyLibExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyLibExample
+//
+//  Created by MAC on 01/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
